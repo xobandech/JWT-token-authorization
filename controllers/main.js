8f81468f0f3747d5fee5a65f30348ff92e7d469f
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-15 14:57:00 +0200

    Create login route controller

--- controllers/main.js
+++ controllers/main.js
@@ -21,7 +21,21 @@ const login = async (req, res) => {
 }
 
 const dashboard = async (req, res) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        throw new CustomAPIError('No token provided', 401)
+    }
 
+    const token = authHeader.split(' ')[1]
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (error) {
+        throw new CustomAPIError('Not authorized to this route', 401)
+    }
+
+    const luckyNumber = Math.floor(Math.random()*100)
+    res.status(200).json({msg: `Hello John Doe`, secret: `Here is your autorized data, lucky number is ${luckyNumber}`})
 }
 
 module.exports = {
