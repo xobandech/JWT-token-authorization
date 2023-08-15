2f50048ff7a5229db6bc99776e1abbfc0acbdfc9
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-15 14:56:27 +0200

    Create login route controller

--- controllers/main.js
+++ controllers/main.js
@@ -1,14 +1,27 @@
+const CustomAPIError = require("../errors/custom-error")
+const jwt = require("jsonwebtoken")
 const login = async (req, res) => {
-    const { password, login } = req.body
-    if (password && login) {
-        
+    const { password, username } = req.body
+    if (!password || !login) {
+        throw new CustomAPIError('Please provide email and password', 400)
     }
-    res.send("Fake login/register route")
+    const id = new Date().getDate()
+
+    // keep payload small, better experience
+    const token = jwt.sign({
+        id,
+        username
+    }, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    })
+    res.status(200).json({
+        msg: "User created",
+        token: token
+    })
 }
 
 const dashboard = async (req, res) => {
-    const luckyNumber = Math.floor(Math.random()*100)
-    res.status(200).json({msg: `Hello John Doe`, secret: `Here is your autorized data, lucky number is ${luckyNumber}`})
+
 }
 
 module.exports = {
