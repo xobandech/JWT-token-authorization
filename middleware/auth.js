3088941248d8ec461a1499263c512cbfee03896f
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-15 18:11:21 +0200

    Create Auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,22 @@
-const authMiddleware = async (req, res, next) => {
-    console.log(req.headers.authorization);
-    next()
-}
+const CustomAPIError = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
 
-module.exports = authMiddleware
+const authMiddleware = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new CustomAPIError("No token provided", 401);
+  }
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const { id, username } = decoded;
+    req.user = { id, username };
+    next();
+  } catch (error) {
+    throw new CustomAPIError("Not authorized to this route", 401);
+  }
+};
+
+module.exports = authMiddleware;
